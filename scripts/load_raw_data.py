7b443f01d47a4bc27b50fbaa476a885dec7eb3dd
Tarek-Radi <noortarak2004@gmail.com>
2026-04-25 10:55:27 +0300

    setup postgres with docker, load raw data, initialize dbt project

---
+++ scripts/load_raw_data.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sqlalchemy import create_engine
+from pathlib import Path
+
+DATA_PATH = Path("datasets")
+
+DB_URL = "postgresql+psycopg2://postgres:postgres@localhost:5433/ecommerce_dw"
+
+TABLES = {
+    "olist_customers_dataset.csv": "customers",
+    "olist_geolocation_dataset.csv": "geolocation",
+    "olist_order_items_dataset.csv": "order_items",
+    "olist_order_payments_dataset.csv": "order_payments",
+    "olist_order_reviews_dataset.csv": "order_reviews",
+    "olist_orders_dataset.csv": "orders",
+    "olist_products_dataset.csv": "products",
+    "olist_sellers_dataset.csv": "sellers",
+    "product_category_name_translation.csv": "product_category_translation",
+}
+
+engine = create_engine(DB_URL)
+
+for file_name, table_name in TABLES.items():
+    file_path = DATA_PATH / file_name
+    print(f"Loading {file_name} -> raw.{table_name}")
+
+    df = pd.read_csv(file_path)
+
+    df.to_sql(
+        table_name,
+        engine,
+        schema="raw",
+        if_exists="replace",
+        index=False
+    )
+
+    print(f"Loaded {len(df)} rows into raw.{table_name}")
+
+print("All raw tables loaded successfully.")
